d407910960729661c7bc89c0fcf638a090a5eb9d
Ty Day <tyrday@gmail.com>
2019-03-02 12:48:09 -0500

    events ordered by event_date

--- front-page.php
+++ front-page.php
@@ -17,16 +17,30 @@
         
         <?php
           $homepageEvents = new WP_Query(array(
-            'posts_per_page' => 2,
-            'post_type' => 'event'
+            'posts_per_page' => -1,
+            'post_type' => 'event',
+            'meta_key' => 'event_date',
+            'orderby'=> 'meta_value_num',
+            'order'=>'asc',
+            'meta_query' => array(
+              array(
+                'key'=> 'event_date',
+                'compare'=> '>=',
+                'value'=> date('Ymd'),
+                'type'=> 'numeric'
+              )
+            )
           ));
 
           while($homepageEvents->have_posts()){
             $homepageEvents->the_post(); ?>
             <div class="event-summary">
           <a class="event-summary__date t-center" href="#">
-            <span class="event-summary__month">Mar</span>
-            <span class="event-summary__day">25</span>  
+            <span class="event-summary__month"><?php 
+              $eventDate = new DateTime(get_field('event_date'));
+              echo $eventDate->format('M')
+            ?></span>
+            <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>  
           </a>
           <div class="event-summary__content">
             <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
